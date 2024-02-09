3b0618a21fb1d63ca96274e3b415f910e6825402
ExLuZiVe53 <imstudent27@gmail.com>
2024-02-09 23:12:40 +0100

    created i18next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,13 @@
+import i18next from 'i18next';
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  ua: { nativeName: 'Ukrainian' },
+};
+
 export const App = () => {
+  const { t } = useTranslation();
   return (
     <div
       style={{
@@ -7,10 +16,23 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <div>
+        {Object.keys(lngs).map(lng => {
+          <button
+            type="submit"
+            key={lng}
+            onClick={() => i18n.changeLanguage(lng)}
+            disabled={i18n.resolvedLanguage === lng.nativeName}
+          >
+            {lngs[lng]}
+          </button>;
+        })}
+      </div>
+      <Trans i18nKey="description">Use React</Trans>
+      {t('Learn')}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import '../src/components/i18n';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
